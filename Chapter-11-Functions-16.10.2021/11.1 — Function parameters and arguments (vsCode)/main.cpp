5091a762996612c66c132aef745fb04207cf6cb2
Mateusz <oureyelet@gmail.com>
2021-10-16 17:16:36 +0100

    -

--- Chapter-11-Functions-16.10.2021/11.1 — Function parameters and arguments (vsCode)/main.cpp
+++ Chapter-11-Functions-16.10.2021/11.1 — Function parameters and arguments (vsCode)/main.cpp
@@ -11,8 +11,15 @@ int main()
     std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
     //////////////////////////////////////////////////////////////////////////////////////////
     /*
+    In Chapter 2, we covered function basics in the following sections:
 
-    
+    2.1 -- Introduction to functions
+    2.3 -- Introduction to function parameters and arguments
+    2.6 -- Forward declarations and definitions
+    2.7 -- Programs with multiple code files
+    2.10 -- Header files
+
+    You should be familiar with the concepts discussed in those lessons before proceeding.
     */
 
 
