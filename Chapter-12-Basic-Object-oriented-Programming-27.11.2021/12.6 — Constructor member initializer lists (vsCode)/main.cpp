ca9055fe23ab970a04325f6e661b9afb5bd37d78
Mateusz <oureyelet@gmail.com>
2021-12-09 19:15:45 +0000

    Non-static member initialization / Constructor

--- Chapter-12-Basic-Object-oriented-Programming-27.11.2021/12.6 — Constructor member initializer lists (vsCode)/main.cpp
+++ Chapter-12-Basic-Object-oriented-Programming-27.11.2021/12.6 — Constructor member initializer lists (vsCode)/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdint> // for std::uint_fast8_t
 
 class Something
 {
@@ -58,6 +59,29 @@ public:
         std::cout << "B " << y << '\n';
     }
 
+};
+
+class RGBA
+{
+private:
+    std::uint_fast8_t m_red{};
+    std::uint_fast8_t m_green{};
+    std::uint_fast8_t m_blue{};
+    std::uint_fast8_t m_alpha{};
+
+public:
+    RGBA() = default;
+
+    RGBA(std::uint_fast8_t red = 0, std::uint_fast8_t green = 0, std::uint_fast8_t blue = 0, std::uint_fast8_t alpha = 255)
+        : m_red{ red }, m_green{ green }, m_blue{ blue }, m_alpha{ alpha }
+    {
+    }
+
+    void print()
+    {
+        std::cout << "red: " << static_cast<int>(m_red) << " green: " << static_cast<int>(m_green) 
+        << " blue: " << static_cast<int>(m_blue) << " alpha: " << static_cast<int>(m_alpha) << '\n';
+    }
 };
 
 int main()
@@ -224,6 +248,114 @@ int main()
     back to the B constructor, and the body of the B constructor executes, printing “B 5”.
     */
 
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Formatting your initializer lists" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    C++ gives you a lot of flexibility in how to format your initializer lists, and it’s really up to you how you’d like to 
+    proceed. But here are some recommendations:
+
+    If the initializer list fits on the same line as the function name, then it’s fine to put everything on one line:
+    
+
+    Something() : m_value1{ 1 }, m_value2{ 2.2 }, m_value3{ 'c' } // everything on one line
+    {
+    }
+
+    If the initializer list doesn’t fit on the same line as the function name, then it should go indented on the next line.
+
+    Something(int value1, double value2, char value3='c') // this line already has a lot of stuff on it
+        : m_value1{ value1 }, m_value2{ value2 }, m_value3{ value3 } // so we can put everything indented on next line
+    {
+    }
+
+    If all of the initializers don’t fit on a single line (or the initializers are non-trivial), then you can space 
+    them out, one per line:
+    
+    Something(int value1, double value2, char value3='c', float value4=34.6f) // this line already has a lot of stuff on it
+        : m_value1{ value1 } // one per line
+        , m_value2{ value2 }
+        , m_value3{ value3 }
+        , m_value4{ value4 }
+    {
+    }
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Initializer list order" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Perhaps surprisingly, variables in the initializer list are not initialized in the order that they are specified in the 
+    initializer list. Instead, they are initialized in the order in which they are declared in the class.
+
+    For best results, the following recommendations should be observed:
+
+    Don’t initialize member variables in such a way that they are dependent upon other member variables being initialized 
+    first (in other words, ensure your member variables will properly initialize even if the initialization ordering is 
+    different).
+    Initialize variables in the initializer list in the same order in which they are declared in your class. This isn’t 
+    strictly required so long as the prior recommendation has been followed, but your compiler may give you a warning if 
+    you don’t do so and you have all warnings turned on.
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Summary" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Member initializer lists allow us to initialize our members rather than assign values to them. This is the only way to 
+    initialize members that require values upon initialization, such as const or reference members, and it can be more 
+    performant than assigning values in the body of the constructor. Member initializer lists work both with fundamental 
+    types and members that are classes themselves.
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Quiz time" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Question #1
+
+    Write a class named RGBA that contains 4 member variables of type std::uint_fast8_t named m_red, m_green, m_blue, 
+    and m_alpha (#include cstdint to access type std::uint_fast8_t). Assign default values of 0 to m_red, m_green, and 
+    m_blue, and 255 to m_alpha. Create a constructor that uses a member initializer list that allows the user to initialize 
+    values for m_red, m_blue, m_green, and m_alpha. Include a print() function that outputs the value of the member variables.
+
+    If you need a reminder about how to use the fixed width integers, please review lesson 4.6 -- Fixed-width integers and size_t.
+
+    Hint: If your print() function isn’t working correctly, make sure you’re casting uint_fast8_t to an int.
+
+    The following code should run:
+
+    int main()
+    {
+        RGBA teal{ 0, 127, 127 };
+        teal.print();
+
+        return 0;
+    }
+
+    and produce the result:
+
+    r=0 g=127 b=127 a=255
+    */
+
+    RGBA teal{ 0, 127, 127};
+    teal.print();
 
     return 0;
 }
