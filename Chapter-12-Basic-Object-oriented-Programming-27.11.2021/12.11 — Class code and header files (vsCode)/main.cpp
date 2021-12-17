7b148594d31ea0514c5fc8aeaa751006d02418c5
Mateusz <oureyelet@gmail.com>
2021-12-17 18:59:02 +0000

    Class code and header files

--- Chapter-12-Basic-Object-oriented-Programming-27.11.2021/12.11 — Class code and header files (vsCode)/main.cpp
+++ Chapter-12-Basic-Object-oriented-Programming-27.11.2021/12.11 — Class code and header files (vsCode)/main.cpp
@@ -133,8 +133,98 @@ int main()
     std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /*
-    
+    It shouldn’t. If your header file has proper header guards, it shouldn’t be possible to include the class definition more 
+    than once into the same file.
+
+    Types (which include classes), are exempt from the part of the one-definition rule that says you can only have one 
+    definition per program. Therefore, there isn’t an issue #including class definitions into multiple code files 
+    (if there was, classes wouldn’t be of much use).
     */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Doesn't defining member functions in the header violate the one-definition rule?" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    It depends. Member functions defined inside the class definition are considered implicitly inline. Inline functions are 
+    exempt from the one definition per program part of the one-definition rule. This means there is no problem defining trivial 
+    member functions (such as access functions) inside the class definition itself.
+
+    Member functions defined outside the class definition are treated like normal functions, and are subject to the one 
+    definition per program part of the one-definition rule. Therefore, those functions should be defined in a code file, 
+    not inside the header. The one exception for this is for template functions, which we’ll cover in a future chapter.
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "So what should I define in the header file vs the cpp file, and what inside the class definition vs outside?" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    You might be tempted to put all of your member function definitions into the header file, inside the class. While this will 
+    compile, there are a couple of downsides to doing so. First, as mentioned above, this clutters up your class definition. 
+    Second, if you change anything about the code in the header, then you’ll need to recompile every file that includes that 
+    header. This can have a ripple effect, where one minor change causes the entire program to need to recompile (which can 
+    be slow). If you change the code in a .cpp file, only that .cpp file needs to be recompiled!
+
+    Therefore, we recommend the following:
+
+    - For classes used in only one file that aren’t generally reusable, define them directly in the single .cpp file they’re 
+    used in.
+    - For classes used in multiple files, or intended for general reuse, define them in a .h file that has the same name as 
+    the class.
+    - Trivial member functions (trivial constructors or destructors, access functions, etc…) can be defined inside the class.
+    - Non-trivial member functions should be defined in a .cpp file that has the same name as the class.
+
+    In future lessons, most of our classes will be defined in the .cpp file, with all the functions implemented directly in the 
+    class definition. This is just for convenience and to keep the examples short. In real projects, it is much more common 
+    for classes to be put in their own code and header files, and you should get used to doing so.
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Default parameters" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Default parameters for member functions should be declared in the class definition (in the header file), where they 
+    can be seen by whomever #includes the header.
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Libraries" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Separating the class definition and class implementation is very common for libraries that you can use to extend your 
+    program. Throughout your programs, you’ve #included headers that belong to the standard library, such as iostream, 
+    string, vector, array, and other. Notice that you haven’t needed to add iostream.cpp, string.cpp, vector.cpp, or array.cpp 
+    into your projects. Your program needs the declarations from the header files in order for the compiler to validate you’re 
+    writing programs that are syntactically correct. However, the implementations for the classes that belong to the C++ standard 
+    library are contained in a precompiled file that is linked in at the link stage. You never see the code.
+
+    Outside of some open source software (where both .h and .cpp files are provided), most 3rd party libraries provide only 
+    header files, along with a precompiled library file. There are several reasons for this: 1) It’s faster to link a precompiled 
+    library than to recompile it every time you need it, 2) a single copy of a precompiled library can be shared by many 
+    applications, whereas compiled code gets compiled into every executable that uses it (inflating file sizes), and 3) 
+    intellectual property reasons (you don’t want people stealing your code).
+
+    Having your own files separated into declaration (header) and implementation (code file) is not only good form, it also 
+    makes creating your own custom libraries easier. Creating your own libraries is beyond the scope of these tutorials, 
+    but separating your declaration and implementation is a prerequisite to doing so.
+    */
+
+
 
     return 0;
 }
