9039c8bba7b179bd0cb3f199c462a259ee4f7b9b
Mateusz <oureyelet@gmail.com>
2021-12-30 10:37:57 +0000

    -

---
+++ Chapter-12-Basic-Object-oriented-Programming-27.11.2021/12.18 — Timing your code (vsCode)/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    /*
+    When writing your code, sometimes you’ll run across cases where you’re not sure whether one method or another will be 
+    more performant. So how do you tell?
+
+    One easy way is to time your code to see how long it takes to run. C++11 comes with some functionality in the chrono 
+    library to do just that. However, using the chrono library is a bit arcane. The good news is that we can easily 
+    encapsulate all the timing functionality we need into a class that we can then use in our own programs.
+
+    Here’s the class:
+    */
+    
+
+    return 0;
+}
