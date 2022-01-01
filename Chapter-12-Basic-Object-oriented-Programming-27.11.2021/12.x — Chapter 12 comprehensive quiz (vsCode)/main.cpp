78ce164e96dc7d2fa98c35c08e9986b1e86a4e9c
Mateusz <oureyelet@gmail.com>
2022-01-01 14:51:08 +0000

    -

--- Chapter-12-Basic-Object-oriented-Programming-27.11.2021/12.x — Chapter 12 comprehensive quiz (vsCode)/main.cpp
+++ Chapter-12-Basic-Object-oriented-Programming-27.11.2021/12.x — Chapter 12 comprehensive quiz (vsCode)/main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <cmath> // for std::sqrt
+
+class Point2d
+{
+private:
+    double m_x;
+    double m_y;
+
+public:
+    Point2d(double x = 0.0, double y = 0.0)
+        : m_x{ x }, m_y{ y }
+    {
+
+    }
+
+    void print()
+    {
+        std::cout << "Point2d(" << m_x << ", " << m_y << ')' << '\n'; 
+    }
+
+    double distanceTo(const Point2d& distance) const
+    {
+        return std::sqrt((m_x - distance.m_x) * (m_x - distance.m_x) + (m_y - distance.m_y) * (m_y - distance.m_y));
+    }
+};
 
 int main()
 {
@@ -72,8 +97,58 @@ int main()
     std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /*
-    
+    Question #1
+
+    a) Write a class named Point2d. Point2d should contain two member variables of type double: m_x, and m_y, 
+    both defaulted to 0.0. Provide a constructor and a print function.
+
+    The following program should run:
+
+    #include <iostream>
+
+    int main()
+    {
+        Point2d first{};
+        Point2d second{ 3.0, 4.0 };
+        first.print();
+        second.print();
+
+        return 0;
+    }
+
+    This should print:
+
+    Point2d(0, 0)
+    Point2d(3, 4)
     */
+
+    Point2d first{};
+    Point2d second{ 3.0, 4.0 };
+
+    first.print();
+    second.print();
+
+    /*
+    b) Now add a member function named distanceTo that takes another Point2d as a parameter, and calculates the distance 
+    between them. Given two points (x1, y1) and (x2, y2), the distance between them can be calculated as 
+    std::sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)). The std::sqrt function lives in header cmath.
+
+    The following program should run:
+    */
+    Point2d first2{};
+    Point2d second2{ 3.0, 4.0 };
+    first2.print();
+    second2.print();
+    std::cout << "Distance between two points: " << first2.distanceTo(second2) << '\n';
+
+    /*
+    c) Change function distanceTo from a member function to a non-member friend function that takes two 
+    Points as parameters. Also rename it “distanceFrom”.
+
+    The following program should run:
+    */
+
+
 
     return 0;
 }
